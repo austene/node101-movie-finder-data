c2b2a33b2db4ab1b468084f818156b74bc2fe1a6
AE <austence@sbcglobal.net>
2019-03-26 15:57:37 -0700

    adds comments to explain code paths

--- server/app.js
+++ server/app.js
@@ -6,14 +6,17 @@ const axios = require('axios');
 const app = express();
 var $ = require('jquery');
 
-debugger;
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
 const port = 3000;
 
+//logs incoming requests through Morgan
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 app.use(morgan('dev', { stream: accessLogStream }));
 
+//Function Definitions
+
+    //hash function
 hashCode = function(s) {
     var h = 0, l = s.length, i = 0;
     if ( l > 0 )
@@ -26,13 +29,14 @@ var hashArray = [];
 for(var i = 0; i < 16; ++i) {
     hashArray.push([]);
 };
-
+    //cache function
 putMovie = function(movieKey, data) {
     var hash = hashCode(movieKey);
     var line = [movieKey, data];
     hashArray[hash].push(line);
 };
 
+    //check cache function and return if correct data is present
 getMovie = function(movieKey) {
     var hash = hashCode(movieKey);
     var bucket = hashArray[hash];
@@ -45,18 +49,16 @@ getMovie = function(movieKey) {
     return returnLine;
 };
 
-
-
-// tt3896198
-
+//CODE
 app.get('/', function(req, res) {
-   //ID code 
+   
+    //code for ID query
     var id = req.query.i;
     if(id != null) {
         var data = getMovie(id);
-        if(data != null) {
+        if(data != null) { //if data present in movie cache, return data
             res.send(data);
-            return;
+            return; //if data not present, go to axios.get
         } else { 
             //get data from OMDb
             axios.get(`http://www.omdbapi.com/?i=${id}&apikey=8730e0e`)
@@ -70,15 +72,15 @@ app.get('/', function(req, res) {
         };
     };
 
-//Title code
-    var title = encodeURIComponent(req.query.t);
+    //code for Title query
+    var title = encodeURIComponent(req.query.t); //need encode for the title to remain same during the mock adapter test
     if(title != null) {
         var data = getMovie(title);
-        if(data != null) {
+        if(data != null) { //if data present in movie cache, return data
             res.send(data);
-            return;
+            return; //if data not present, go to axios.get
         };  
- 
+        //get data from OMDb
         axios.get(`http://www.omdbapi.com/?t=${title}&apikey=8730e0e`)
             .then(function (response) {
                 putMovie(title, response.data);
